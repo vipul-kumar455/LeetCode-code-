9e23902aafbebd4841949ef8b940bd98085eabf0
Vipul Kumar <74072895+vipul-kumar455@users.noreply.github.com>
2023-06-05 02:12:01 +0530

    Update 169. Majority Element.cpp

--- 365_days/169. Majority Element.cpp
+++ 365_days/169. Majority Element.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       map<int,int>mp;
+      //  map<int,int>mp;
        int n=nums.size();
-        for(int i=0;i<n;i++)
+      //   for(int i=0;i<n;i++)
+      //   {
+      //     mp[nums[i]]++;
+      //   } 
+      //   for(auto i:mp)
+      //   {
+      //     if(i.second>n/2)
+      //     return i.first;
+      //   }
+      //   return -1;
+        //moore's algorithm
+        /// o(n) and o(1)
+      int cnt=0,maj;
+      for(int i=0;i<n;i++)
+      {
+        if(cnt)
         {
-          mp[nums[i]]++;
-        } 
-        for(auto i:mp)
-        {
-          if(i.second>n/2)
-          return i.first;
+          cnt+=(nums[i]==maj?1:-1);
         }
-        return -1;
+        else
+        {
+          maj=nums[i];
+          cnt=1;
+        }
+      }
+      return maj;
     }
 };
